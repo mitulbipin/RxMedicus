536ebaadacb940b947bb29bbd1c0401e963fea07
mitul bipin <mitulbipin24@gmail.com>
2021-04-11 15:48:32 +0530

    Handled the crash issue in UserProfile Activity and formatted the code.

--- app/src/main/java/com/example/rxmedicus1/SecondActivity.java
+++ app/src/main/java/com/example/rxmedicus1/SecondActivity.java
@@ -34,16 +34,110 @@ import com.squareup.picasso.Picasso;
 import java.io.IOException;
 import java.util.BitSet;
 
-public class SecondActivity extends AppCompatActivity
-{
-private FirebaseAuth firebaseAuth;
-private FirebaseDatabase firebaseDatabase;
-private FirebaseStorage firebaseStorage;
-private StorageReference storageReference;
-private TextView UserNameDisp,Pname,Pdob,PAge,Paddr,Pnumb,BloodGroup,Pgend;
-private Button uploadButton;
-private ImageView ProfilePic;
-public void variables(){
+public class SecondActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+    private TextView UserNameDisp, Pname, Pdob, PAge, Paddr, Pnumb, BloodGroup, Pgend;
+    private Button uploadButton;
+    private ImageView ProfilePic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        variables();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+        storageReference.child(firebaseAuth.getUid()).child("Images").getDownloadUrl()
+                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).fit().centerCrop().into(ProfilePic);
+                    }
+                });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.PatientBtmNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.BtmNavHospital: {
+                        startActivity(new Intent(SecondActivity.this, HospitalActivity.class));
+                        break;
+                    }
+                    case R.id.BtmNavDoctor: {
+                        startActivity(new Intent(SecondActivity.this, DoctorListActivity.class));
+                        break;
+                    }
+                    case R.id.BtmNavPatientUpload: {
+                        startActivity(new Intent(SecondActivity.this, UserProfile.class));
+                        break;
+                    }
+                    case R.id.BtmNavMedicine:
+                        break;
+                    case R.id.BtmNavDisease:
+                        break;
+                }
+                return false;
+            }
+        });
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    UsersProfile usersProfile = dataSnapshot.getValue(UsersProfile.class);
+                    Pname.setText("Name (As per Aadhaar):" + usersProfile.getUsername());
+                    Pdob.setText("Date of birth:" + usersProfile.getDateofbirth());
+                    PAge.setText("Age:" + usersProfile.getAge());
+                    Paddr.setText("Address (As per Aadhaar):" + usersProfile.getAddress());
+                    Pnumb.setText("Contact Number:" + usersProfile.getNumber());
+                    Pgend.setText("Gender:" + usersProfile.getGender());
+                    BloodGroup.setText("Blood Group:" + usersProfile.getBloodgroup());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(SecondActivity.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        UserNameDisp.setText("Hi " + firebaseAuth.getCurrentUser().getEmail().replace("@gmail.com", ""));
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SecondActivity.this, UserProfile.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    } //menu bar method 1
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { //menu bar method 2
+        switch (item.getItemId()) {
+            case R.id.LogoutMenu: {
+                logout();
+                break;
+            }
+            case R.id.UploadMedicationMenu: {
+                startActivity(new Intent(SecondActivity.this, MedicationActivity.class));
+
+            }
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    public void variables() {
         Pname = findViewById(R.id.ProfileName);
         Pdob = findViewById(R.id.ProfileDob);
         PAge = findViewById(R.id.ProfileAge);
@@ -55,99 +149,12 @@ public void variables(){
         UserNameDisp = findViewById(R.id.UserDisp);
         uploadButton = findViewById(R.id.SeconActivityButton);
     }
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
-        variables();
-            firebaseAuth = FirebaseAuth.getInstance();
-            firebaseDatabase =FirebaseDatabase.getInstance();
-            DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
-            firebaseStorage = FirebaseStorage.getInstance();
-              storageReference = firebaseStorage.getReference();
-              storageReference.child(firebaseAuth.getUid()).child("Images").getDownloadUrl()
-                      .addOnSuccessListener(new OnSuccessListener<Uri>() {
-                  @Override
-                  public void onSuccess(Uri uri) {
-                      Picasso.get().load(uri).fit().centerCrop().into(ProfilePic);
-                  }
-              });
 
-              BottomNavigationView bottomNavigationView = findViewById(R.id.PatientBtmNavigation);
-              bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-                  @Override
-                  public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                      switch (menuItem.getItemId()){
-                          case R.id.BtmNavHospital:{
-                              startActivity(new Intent(SecondActivity.this,HospitalActivity.class));break;}
-                          case R.id.BtmNavDoctor:{
-                              startActivity(new Intent(SecondActivity.this,DoctorListActivity.class));break;}
-                          case R.id.BtmNavPatientUpload:
-                          {startActivity(new Intent(SecondActivity.this,UserProfile.class));break;}
-                          case R.id.BtmNavMedicine: break;
-                          case R.id.BtmNavDisease: break;
-                      }
-                      return false;
-                  }
-              });
-        databaseReference.addValueEventListener(new ValueEventListener() {
-    @Override
-    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-        if(dataSnapshot.exists())
-        {
-            UsersProfile usersProfile = dataSnapshot.getValue(UsersProfile.class);
-            Pname.setText("Name (As per Aadhaar):" + usersProfile.getUsername());
-            Pdob.setText("Date of birth:" + usersProfile.getDateofbirth());
-            PAge.setText("Age:" + usersProfile.getAge());
-            Paddr.setText("Address (As per Aadhaar):" + usersProfile.getAddress());
-            Pnumb.setText("Contact Number:" + usersProfile.getNumber());
-            Pgend.setText("Gender:" + usersProfile.getGender());
-            BloodGroup.setText("Blood Group:" + usersProfile.getBloodgroup());
-        }
+    private void logout() {
+        firebaseDatabase.goOffline();
+        firebaseAuth.signOut();
+        Toast.makeText(this, "Logout Successful", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(SecondActivity.this, MainActivity.class));
+        finish(); //15th May 2019
     }
-    @Override
-    public void onCancelled(@NonNull DatabaseError databaseError) {
-        Toast.makeText(SecondActivity.this,databaseError.getCode(),Toast.LENGTH_SHORT).show();
-    }
-});
-        UserNameDisp.setText("Hi " +firebaseAuth.getCurrentUser().getEmail().replace("@gmail.com",""));
-        uploadButton.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        startActivity(new Intent(SecondActivity.this,UserProfile.class));
-                }
-});
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
-        return super.onCreateOptionsMenu(menu);
-    } //menu bar method 1
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) { //menu bar method 2
-        switch (item.getItemId()) {
-            case R.id.LogoutMenu: {
-              logout();
-              break;
-            }
-            case R.id.UploadMedicationMenu:
-            {
-                startActivity(new Intent(SecondActivity.this,MedicationActivity.class));
-
-            }
-            return super.onOptionsItemSelected(item);
-        }
-        return true;
-    }
-private void logout()
-{
-    firebaseDatabase.goOffline();
-    firebaseAuth.signOut();
-    Toast.makeText(this, "Logout Successful", Toast.LENGTH_SHORT).show();
-    startActivity(new Intent(SecondActivity.this, MainActivity.class));
-    finish(); //15th May 2019
-}
 }
--- app/src/main/java/com/example/rxmedicus1/UserProfile.java
+++ app/src/main/java/com/example/rxmedicus1/UserProfile.java
@@ -1,4 +1,5 @@
 package com.example.rxmedicus1;
+
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -38,49 +39,47 @@ import java.io.IOException;
 import java.util.Calendar;
 
 public class UserProfile extends AppCompatActivity {
-    public static final String TAG ="UserProfile"; // Date of birth
-public FirebaseAuth firebaseAuth;
-public FirebaseDatabase firebaseDatabase;
-public FirebaseStorage firebaseStorage;
-public StorageReference storageReference;
-public EditText username,age,address,income,mobno,BloodGroup;
-public ImageView userImage;
-public String DateofBirth;
-public String gender,NameString,AgeString,AddressString,DOBString,UserIncome,UserNumber,BGroup;
-public RadioGroup rgGender;
-public RadioButton rbOption;
-public Button UploadButton,PDF,AdhaarUpload;
-public Button InsuranceUpload;
-public TextView DisplayDate;
-    private static int PICK_IMAGE = 123,PICK_DOCUMENT=999,PICK_DOCUMENT1=998;
-    Uri imagePath,PDFPath,Document1Path;
+    public static final String TAG = "UserProfile"; // Date of birth
+    public FirebaseAuth firebaseAuth;
+    public FirebaseStorage firebaseStorage;
+    public StorageReference storageReference;
+    public EditText username, age, address, income, mobno, BloodGroup;
+    public ImageView userImage;
+    public String DateofBirth;
+    public String gender, NameString, AgeString, AddressString, DOBString, UserIncome, UserNumber, BGroup;
+    public RadioGroup rgGender;
+    public RadioButton rbOption;
+    public Button UploadButton, PDF, AdhaarUpload;
+    public Button InsuranceUpload;
+    public TextView DisplayDate;
+    private static int PICK_IMAGE = 123, PICK_DOCUMENT = 999, PICK_DOCUMENT1 = 998;
+    public DatePickerDialog.OnDateSetListener DateSetListener;
+    Uri imagePath, PDFPath, Document1Path;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode ==PICK_IMAGE && resultCode == RESULT_OK && data.getData() != null){
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data.getData() != null) {
             imagePath = data.getData();
             try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),imagePath);
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imagePath);
                 userImage.setImageBitmap(bitmap);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-        else if(requestCode == PICK_DOCUMENT && resultCode == RESULT_OK && data.getData() != null){
+        } else if (requestCode == PICK_DOCUMENT && resultCode == RESULT_OK && data.getData() != null) {
             PDFPath = data.getData();
-        }
-        else if(requestCode == PICK_DOCUMENT1 && resultCode == RESULT_OK && data.getData() != null){
+        } else if (requestCode == PICK_DOCUMENT1 && resultCode == RESULT_OK && data.getData() != null) {
             Document1Path = data.getData();
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
-    public DatePickerDialog.OnDateSetListener DateSetListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
         firebaseAuth = FirebaseAuth.getInstance();
-        firebaseStorage =FirebaseStorage.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
         storageReference = firebaseStorage.getReference();
 
         username = findViewById(R.id.tvName);
@@ -103,36 +102,42 @@ public TextView DisplayDate;
                 int year = cal.get(Calendar.YEAR);
                 int month = cal.get(Calendar.MONTH);
                 int day = cal.get(Calendar.DAY_OF_MONTH);
-                DatePickerDialog dialog = new DatePickerDialog(UserProfile.this,android.R.style.Theme_Black,DateSetListener,month,day,year);
+                DatePickerDialog dialog = new DatePickerDialog(UserProfile.this, android.R.style.Theme_Black, DateSetListener, month, day, year);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
             }
         });
-        DateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-               month++;
-                Log.d(TAG,"Date of Birth " +dayOfMonth+"/"+month+"/"+year);
-                DateofBirth = dayOfMonth +"/"+month+"/"+year;
-                DisplayDate.setText(DateofBirth);
-            }
-        };
+
+        try {
+            DateSetListener = new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    month++;
+                    Log.d(TAG, "Date of Birth " + dayOfMonth + "/" + month + "/" + year);
+                    DateofBirth = dayOfMonth + "/" + month + "/" + year;
+                    DisplayDate.setText(DateofBirth);
+                }
+            };
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         userImage.setOnClickListener(new View.OnClickListener() { //picture upload
-             @Override
-              public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setType("image/*"); //application/*
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent,"Select Image "), PICK_IMAGE);
-                }
-                 });
+                startActivityForResult(Intent.createChooser(intent, "Select Image "), PICK_IMAGE);
+            }
+        });
         PDF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent1 = new Intent();
                 intent1.setType("application/pdf"); //application/*
                 intent1.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent1,"Select the file "), PICK_DOCUMENT);
+                startActivityForResult(Intent.createChooser(intent1, "Select the file "), PICK_DOCUMENT);
             }
         });
         AdhaarUpload.setOnClickListener(new View.OnClickListener() {
@@ -141,7 +146,7 @@ public TextView DisplayDate;
                 Intent intent1 = new Intent();
                 intent1.setType("application/pdf"); //application/*
                 intent1.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent1,"Select the file "), PICK_DOCUMENT1);
+                startActivityForResult(Intent.createChooser(intent1, "Select the file "), PICK_DOCUMENT1);
             }
         });
         InsuranceUpload.setOnClickListener(new View.OnClickListener() {
@@ -151,22 +156,27 @@ public TextView DisplayDate;
                 Toast.makeText(UserProfile.this, "Insurance uploaded successfully", Toast.LENGTH_SHORT).show();
             }
         });
-         UploadButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            sendUserData();
-            ImageStorage();
-            Document1Storage();
-            Toast.makeText(UserProfile.this, "Data Successfully Uploaded!", Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(UserProfile.this, SecondActivity.class));}
-    });
+
+        UploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    sendUserData();
+                    ImageStorage();
+                    Document1Storage();
+                    Toast.makeText(UserProfile.this, "Data Successfully Uploaded!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(UserProfile.this, SecondActivity.class));
+                } catch (Exception e) {
+                    Toast.makeText(UserProfile.this, "Add the missing fields", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         rgGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                rbOption = rgGender.findViewById(checkedId) ;
-                switch (checkedId)
-                {
+                rbOption = rgGender.findViewById(checkedId);
+                switch (checkedId) {
                     case R.id.rbMale:
                         gender = rbOption.getText().toString().trim();
                     case R.id.rbFemale:
@@ -177,8 +187,8 @@ public TextView DisplayDate;
             }
         });
     }
-    private void sendUserData()
-    {
+
+    private void sendUserData() {
         NameString = username.getText().toString().trim();
         AgeString = age.getText().toString().trim();
         AddressString = address.getText().toString();
@@ -190,11 +200,11 @@ public TextView DisplayDate;
 
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference myRef = firebaseDatabase.getReference(firebaseAuth.getUid());
-        UsersProfile usersProfile = new UsersProfile(NameString,AgeString,gender,AddressString,DOBString,UserIncome,UserNumber,BGroup);
+        UsersProfile usersProfile = new UsersProfile(NameString, AgeString, gender, AddressString, DOBString, UserIncome, UserNumber, BGroup);
         myRef.setValue(usersProfile);
     }
 
-    public void ImageStorage(){
+    public void ImageStorage() {
         StorageReference imageRef = storageReference.child(firebaseAuth.getUid()).child("Images");
         UploadTask uploadTask = imageRef.putFile(imagePath);
         uploadTask.addOnFailureListener(new OnFailureListener() {
@@ -209,8 +219,8 @@ public TextView DisplayDate;
             }
         });
     }
-    public void DocumentStorage()
-    {
+
+    public void DocumentStorage() {
         StorageReference pdfRef = storageReference.child(firebaseAuth.getUid()).child("Insurance");
         UploadTask uploadTask1 = pdfRef.putFile(PDFPath);
         uploadTask1.addOnFailureListener(new OnFailureListener() {
@@ -226,8 +236,7 @@ public TextView DisplayDate;
         });
     }
 
-    public void Document1Storage()
-    {
+    public void Document1Storage() {
         StorageReference pdfRef = storageReference.child(firebaseAuth.getUid()).child("AdhaarUpload");
         pdfRef.putFile(Document1Path);
     }
